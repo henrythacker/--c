830615fac214cede4402043d22867ca125f36cdf
Henry Thacker <henry@henrythacker.com>
2009-12-19 19:10:20 +0000

    More useful test of IF statements

--- examples/iftest.c
+++ examples/iftest.c
@@ -1,13 +1,23 @@
 int main() {
 	int a = 10;
 	if (1) {
-		a = 5;
+		if (0) {
+			a = 5;
+		}
+		else {
+			a = 12;
+		}
 	}
 	else {
 		a = 9;
 	}
-	if (0) {
-		a = a - 1;
+	if (1) {
+		if (0) {
+			a = a - 1;
+		}
+		else {
+			a = a - 2;
+		}
 	}
 	else {
 		a = a - 3;
