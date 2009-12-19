e88a6356f4b252f44f17cb857d5452d71e5bb7c2
Henry Thacker <henry@henrythacker.com>
2009-12-19 14:06:43 +0000

    Added requisite utility functions

---
+++ tacgen/utilities.c
@@ -0,0 +1,40 @@
+#include "utilities.h"
+
+/* Assertion with error text */
+void assert(int assertion, char *error) {
+	if (!assertion) {
+		printf("TAC Error: %s\n", error);
+		exit(-1);
+	}
+}
+
+/* Initialise a void placeholder value */
+value *void_value(void) {
+	static int t_count = 0;
+	value *tmp_value = calloc(1, sizeof(value));
+	char temporary_name[10];
+	sprintf(temporary_name, "void%d", t_count);
+	tmp_value->identifier = temporary_name;
+	tmp_value->value_type = VT_VOID;
+	t_count++;
+	return tmp_value;
+}
+
+/* Check that a variable with value: variable_value matches up with the declared type */
+void type_check_assignment(value *variable_name, value *variable_value, int declared_type) {
+	if (!variable_name || !variable_value) fatal("Not enough information to typecheck statement!");
+	/* VOID variables can never be assigned to! */
+	if (declared_type == VOID) {
+		fatal("Variable '%s' of type \"void\" can not be assigned a value", to_string(variable_name));	
+	}
+	if (declared_type == INT) {
+		if (variable_value->value_type != VT_INTEGR) {
+			fatal("Variable '%s' of type \"int\" can not be assigned a non-integer value", to_string(variable_name));	
+		}
+	}
+	if (declared_type == FUNCTION) {
+		if (variable_value->value_type != VT_FUNCTN) {
+			fatal("Variable '%s' of type \"function\" can only point to functions", to_string(variable_name));
+		}
+	}
+}
---
+++ tacgen/utilities.h
@@ -0,0 +1,17 @@
+#ifndef __UTIL_H
+#define __UTIL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "C.tab.h"
+#include "nodes.h"
+#include "conversion.h"
+#include "token.h"
+
+/* Fn prototypes */
+void assert(int, char *);
+void type_check_assignment(value *, value *, int);
+value *void_value(void);
+
+#endif
