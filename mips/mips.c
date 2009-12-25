a1aefdb1f2ad90a0557bf0a0eb0603b2643f6fec
Henry Thacker <henry@henrythacker.com>
2009-12-25 21:26:10 +0000

    Write preamble

--- mips/mips.c
+++ mips/mips.c
@@ -1,6 +1,15 @@
 #include "mips.h"
 
+void write_preamble() {
+	struct tm *local_time;
+	time_t gen_time;
+	gen_time = time(NULL);
+	local_time = localtime(&gen_time);
+	printf("# Compiled from --C\n# %s\n\n", asctime(local_time));
+}
+
 /* Generate MIPS code for given tree */
 void code_gen(NODE *tree) {
-	
+	tac_quad *quad = start_tac_gen(tree);
+	write_preamble();
 }
--- mips/mips.h
+++ mips/mips.h
@@ -1,6 +1,7 @@
 #ifndef __MIPS_H
 #define __MIPS_H
 
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
