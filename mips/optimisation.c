46117509036b313db75ccd215a33abdbbf920f90
Henry Thacker <henry@henrythacker.com>
2010-01-05 23:33:47 +0000

    Redundant move optimisation

--- mips/optimisation.c
+++ mips/optimisation.c
@@ -38,8 +38,39 @@ mips_instruction *remove_after_return(mips_instruction *input) {
 	return input;
 }
 
+/* Due to the way the code generation happens, often one can find the pattern move $X, $v0 followed directly by move $v0, $X */
+/* Remove the redundant second line, the first is enough */
+/* Rationale: remove redundant code */
+mips_instruction *remove_redundant_move(mips_instruction *input) {
+	mips_instruction *c_ins;
+	mips_instruction *p_ins;
+	p_ins = NULL;
+	c_ins = input;
+	while (c_ins) {
+		if (c_ins && p_ins) {
+			int prev;
+			int curr;
+			prev = strcmp(p_ins->operation, "move")==0 && p_ins->operand2 && p_ins->operand2_type == OT_REGISTER && p_ins->operand2->reg == $v0;
+			curr = strcmp(c_ins->operation, "move")==0 && c_ins->operand1 && c_ins->operand1_type == OT_REGISTER && c_ins->operand1->reg == $v0;
+			if (prev && curr && p_ins->operand2->reg == c_ins->operand1->reg) {
+				/* Target couple of instructions found */
+				/* Circumvent the current instruction */
+				p_ins->next = c_ins->next;
+				free(c_ins);
+				c_ins = p_ins->next;
+				p_ins = c_ins->next;
+				continue;
+			}
+		}
+		p_ins = c_ins;
+		c_ins = c_ins->next;
+	}
+	return input;
+}
+
 /* Perform optimisation functions */
 mips_instruction *do_optimise(mips_instruction *input) {
 	input = remove_after_return(input);
+	input = remove_redundant_move(input);
 	return input;
 }
